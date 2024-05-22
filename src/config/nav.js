46d4aef6baa412d1980f50f29faf3bf1ac4bd456
x0ba <hey@x0ba.net>
2024-05-21 21:11:23 -0700

    init

---
+++ src/config/nav.js
@@ -0,0 +1,14 @@
+export const nav = [
+	{
+		title: 'Home',
+		slug: '/',
+	},
+	{
+		title: 'About',
+		slug: '/company/about',
+	},
+	{
+		title: 'Contact',
+		slug: '/company/contact',
+	},
+];
---
+++ src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+	title: `MHS Code Quest`,
+	description: `Home page for Milpitas High School Code Quest`,
+	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
+	name: `MHS Code Quest`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	enableThemeSwitcher: false,
+	showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+};
